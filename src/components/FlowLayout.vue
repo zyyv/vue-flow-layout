7bfb5e26c9fbc6437f4582a354bfd880e0c34ddc
Chris <hizyyv@gmail.com>
2024-10-03 17:17:00 +0800

    feat: complete component

--- src/components/FlowLayout.vue
+++ src/components/FlowLayout.vue
@@ -1,6 +1,5 @@
 <script lang='ts' setup>
-import type { VNode } from 'vue'
-import { onMounted, ref, withDefaults } from 'vue'
+import { onMounted, onUnmounted, ref, watch, withDefaults } from 'vue'
 
 defineOptions({
   name: 'FlowLayout',
@@ -14,24 +13,118 @@ const props = withDefaults(defineProps<{
   gap: 4,
 })
 
-const slots = defineSlots<{
+defineSlots<{
   default: () => any
 }>()
 
+const prevW = '$$symbol:prevW'
+const prevH = '$$symbol:prevH'
+
+let sizeObs: ResizeObserver
+let childrenObs: MutationObserver
+
 const layout = ref<HTMLDivElement>()
+const layouting = ref(false)
+
+function reLayout() {
+  if (layouting.value)
+    return
+  layouting.value = true
+
+  if (!layout.value)
+    return
+
+  requestAnimationFrame(() => {
+    try {
+      const parent = layout.value
+      const children = Array.from(parent.children) as HTMLElement[]
+      const len = children.length
+      const [gapX, gapY] = Array.isArray(props.gap) ? props.gap : [props.gap, props.gap]
+
+      const colWidth = (parent.offsetWidth - (props.cols - 1) * gapX) / props.cols
+      for (const el of children) {
+        el.style.width = `${colWidth}px`
+      }
+
+      const hs = Array.from({ length: len }, (_, index) => {
+        const el = children[index]
+        el[prevW] = el.offsetWidth
+        el[prevH] = el.offsetHeight
+        return el.offsetHeight
+      })
+
+      const columnHs = Array.from({ length: props.cols }, () => 0)
+
+      for (let i = 0; i < len; i++) {
+        const el = children[i]
+        const minColHeight = Math.min(...columnHs)
+        const colIndex = columnHs.indexOf(minColHeight)
+        const left = colIndex * (colWidth + gapX)
+        const top = minColHeight
+
+        el.style.left = `${left}px`
+        el.style.top = `${top}px`
+
+        columnHs[colIndex] += hs[i] + gapY
+      }
+
+      parent.style.height = `${Math.max(...columnHs) - gapY}px`
+    }
+    finally {
+      layout.value[prevW] = layout.value.offsetWidth
+      layout.value[prevH] = layout.value.offsetHeight
+      layouting.value = false
+    }
+  })
+}
+
+function sizeListener() {
+  const parent = layout.value!
+  sizeObs = new ResizeObserver((els) => {
+    for (const { target: el } of els) {
+      if (el.offsetWidth !== el[prevW] || el.offsetHeight !== el[prevH]) {
+        reLayout()
+      }
+    }
+  })
+  sizeObs.observe(parent)
+  for (const el of parent.children) {
+    sizeObs.observe(el)
+  }
+}
+
+function childrenLisener() {
+  const parent = layout.value!
+  childrenObs = new MutationObserver((ml) => {
+    for (const m of ml) {
+      for (const el of m.addedNodes) {
+        if (el.nodeType === 1) {
+          sizeObs.observe(el as HTMLElement)
+        }
+      }
+      for (const el of m.removedNodes) {
+        if (el.nodeType === 1) {
+          sizeObs.unobserve(el as HTMLElement)
+        }
+      }
+    }
+
+    reLayout()
+  })
+  childrenObs.observe(parent, { childList: true, subtree: true })
+}
+
+watch(() => [props.cols, props.gap], reLayout)
 
 onMounted(() => {
-  const children = slots.default()
+  reLayout()
+  sizeListener()
+  childrenLisener()
+})
 
-  children.forEach((vnode: VNode<HTMLElement>, index: number) => {
-    const col = index % props.cols
-    const row = Math.floor(index / props.cols)
-    const gap = Array.isArray(props.gap) ? props.gap : [props.gap, props.gap]
-    const el = vnode.el!
-
-    el.style.left = `${col * (el.offsetWidth + gap[0])}px`
-    el.style.top = `${row * (el.offsetHeight + gap[1])}px`
-  })
+onUnmounted(() => {
+  sizeObs.disconnect()
+  childrenObs.disconnect()
 })
 </script>
 
@@ -52,5 +145,6 @@ onMounted(() => {
 #flow-layout > * {
   position: absolute;
   box-sizing: border-box;
+  transition: all 350ms ;
 }
 </style>
